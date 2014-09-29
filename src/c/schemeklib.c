1406c4cf39a48980a6ada424b044c8ea29faccea
Mike Appleby <mike@app.leby.org>
2014-09-29 14:08:49 -0500

    Comment out unused variable rcsid.

--- src/c/schemeklib.c
+++ src/c/schemeklib.c
@@ -23,10 +23,9 @@
 #include "scheme.h"
 
 /* Identify this library.
+ * static char *rcsid =
+ *  "@(#)$Id: schemeklib.c,v 4.0 1995/07/10 06:52:31 queinnec Exp $";
  */
-
-static char *rcsid = 
-  "@(#)$Id: schemeklib.c,v 4.0 1995/07/10 06:52:31 queinnec Exp $";
 
 /* Global constants
  */
